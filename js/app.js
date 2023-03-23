6cd2c7272392a28eb2cde86fac1dcda8bf74a8ef
cameron <hammerbro_97@hotmail.com>
2023-03-23 17:37:00 -0600

    added selectors

--- js/app.js
+++ js/app.js
@@ -1,5 +1,9 @@
-let first_p_tag = document.querySelector(`p`);
+let first_h3_tag = document.querySelector(`h3`);
 
 let ID_finder = document.querySelector(`#header`);
 
 let grab_class = document.querySelectorAll(`.text`);
+
+let ID_finder_Element = document.getElementById(`#header`);
+ 
+let class_finder_element = document.getElementsByClassName(`.text`);
