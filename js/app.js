9791e0ebb8ad6a4c20f39abc7cab2f5a2a49602a
cameron <hammerbro_97@hotmail.com>
2023-03-23 19:14:03 -0600

    hacked loop with attempted function hack loop

--- js/app.js
+++ js/app.js
@@ -2,8 +2,38 @@ let first_h3_tag = document.querySelector(`h3`);
 
 let ID_finder = document.querySelector(`#header`);
 
+ID_finder[`style`][`fontSize`] = `76px`;
+ID_finder[`style`][`fontWeight`] = `800`;
+ID_finder[`style`][`width`] = `1%`;
+
 let grab_class = document.querySelectorAll(`.text`);
 
-let ID_finder_Element = document.getElementById(`#header`);
+function hacked(hacktext){
+
+
+
+
+for(let counter = 0; counter < hacktext.length; counter = counter +1){
+
+if(hacktext === true){
+grab_class[counter][`innerHTML`] = `hacked` ;
+
+
+} 
+
+}
+
+
+}
+
+let insert_text = hacked(true);
+
+for(let counter = 0; counter < grab_class.length; counter = counter +1){
+    grab_class[counter][`innerHTML`] = `hacked`;
+    
+    }
+    
+    
+let ID_finder_Element = document.getElementById(`span`);
  
-let class_finder_element = document.getElementsByClassName(`.text`);
+let class_finder_element = document.getElementsByClassName(`text_2`);
